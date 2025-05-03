b712b4a1983898c63cb9eb7977f24145ee535944
Khalidrika <rikarikamika2002@gmail.com>
2025-05-03 20:29:54 +0100

    dd

--- frontend/js/routes.js
+++ frontend/js/routes.js
@@ -1,27 +1,34 @@
 const routes = {
-    '/login': 'login',
-    '/register': 'register',
-    '/home': 'home',
+  '/login': 'login',
+  '/register': 'register',
+  '/home': 'home',
 }; 
 
 export function navigate(path) {
-    history.pushState({}, '', path);
-   // renderPage(path);
+  history.pushState({}, '', path);
+  renderPage(path);
 }
 
 
-function renderPage(path) {
-    const page = routes[path] || 'login';
+export function renderPage(path) {
+  const page = routes[path] || 'login';
 
-    //document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
-   // document.getElementById(page).classList.add('active');
+  //document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
+ // document.getElementById(page).classList.add('active');
 
-    if (page === 'home') {
-      const user = sessionStorage.getItem('loggedIn');
-      if (!user) {
-        navigate('/login');
-      } else {
-        document.getElementById('welcomeUser').innerText = `Hello, ${user}`;
-      }
+  if (page === 'home') {
+    const user = sessionStorage.getItem('loggedIn');
+    if (!user) {
+      navigate('/login');
+    } else {
+      document.getElementById('welcomeUser').innerText = `Hello, ${user}`;
     }
+  }
+  document.querySelectorAll('.page').forEach(p => p.classList.remove('active'));
+  const el = document.getElementById(page);
+  if (el) el.classList.add('active');
+  window.addEventListener('popstate', ()=> {
+    renderPage(window.location.pathname);
+  })
+
 }
